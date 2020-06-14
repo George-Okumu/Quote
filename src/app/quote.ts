b7437f669fd53716e9ee898636cac571fcb494f4
George Okumu <gokumu12@gmail.com>
2020-06-14 15:00:51 +0300

    created a constructor to take our quote details,added clicking event emitter:a user can toggle between quote details

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,7 @@
 export class Quote {
+    showDescription: boolean;
+    author: string;
+    constructor(public name: string, public description: string, public postDate: Date){
+        this.showDescription = false;
+    }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes: Quote[] = [
+    new Quote('Gregory Issac', 'No success without commitment', new Date(2020,6,14)),
+  ];
+
+  toggleDetails(index) {
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+  deleteQuote(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
+      if (toDelete) {
+        this.quotes.splice(index, 1);
+      }
+    }
+  }
 
   constructor() { }
 
